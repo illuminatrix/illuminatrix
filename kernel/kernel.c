81b792ddc76ba00d83f216c52f224d9c1242ddd0
Perez Gonzalez, Sergio <sperezglz@gmail.com>
2019-08-20 23:26:35 -0500

    Add idt setup for first 32 vectors

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "kernel.h"
+#include "interrupts.h"
 
 char *message = "hello kernel";
 
@@ -9,4 +10,6 @@ void kernel_main(multiboot_info_t *mem_info_ptr)
     uint8_t i = 0;
     for (i = 0; message[i] != 0; i++)
         t_buffer[i] = (uint16_t)message[i] | (uint16_t) 15 << 8;
+
+    load_idt();
 }
