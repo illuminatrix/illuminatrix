ae01770d17b5dbac865dc338f7e91e2239ba1888
Perez Gonzalez, Sergio <sperezglz@gmail.com>
2019-08-22 00:29:35 -0500

    Change flags in GDT entries, add macro to print from pmode
    GDT entries flag changes:
      - Extended segment limit 19:16 from 0x0 to 0xf
      - Set granularity flag 'G' for segment limit interpretation as
        4Kb units
      - Set 'D/B' flag for 32-bit segment
    
    Also add make rule to generate disassembly
    
    Signed-off-by: Perez Gonzalez, Sergio <sperezglz@gmail.com>

--- boot/boot.h
+++ boot/boot.h
@@ -23,3 +23,17 @@
         mov $0x01, %al // move cursor when print string
         int $0x10
 .endm
+
+.macro PRINT_STR_PMODE str  // This macro doesn't know where cursor is located when it is called,
+        movw \str, %si      // other macros are needed to clean the screen, position cursor, etc.
+        mov $0xb8000, %ebx
+1:
+        lodsb
+        orb %al,%al
+        jz 2f
+        orl $0x0100, %eax
+        movw %ax, (%ebx)
+        add $2, %ebx
+        jmp 1b
+2:
+.endm
