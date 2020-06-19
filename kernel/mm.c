68b4ab915f1278ae6e9bbfd57c73feeb16090ed6
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2020-06-19 17:23:21 -0500

    Fix double symbl linking issue
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- kernel/mm.c
+++ kernel/mm.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include "mm.h"
 
+uint32_t pdir[DIR_SIZE] __attribute__((aligned(FRAME)));
+uint32_t pt[DIR_SIZE]   __attribute__((aligned(FRAME)));
+
 void setup_identity_paging(void)
 {
     uint32_t i;
@@ -21,4 +24,15 @@ void init_mm(mmap_entry_t *mmap_addr, uint32_t len)
 
     setup_identity_paging();
     __asm__ volatile ("movl $0x1234, 0"); /* remove me */
+}
+
+void turn_on_paging(void)
+{
+    __asm__ volatile ("mov %0, %%eax\n\t"
+                      "mov %%eax, %%cr3\n\t"
+                      "mov %%cr0, %%eax\n\t"
+                      "or $0x80000000, %%eax\n\t"
+                      "mov %%eax, %%cr0\n"
+                      :: "p" (pdir)
+                      : "ax");
 }
