d250cb8904a616bb01902dad137cde737cbf2f4b
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2021-03-04 22:08:03 -0600

    change printf implementation to use vsprintf
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- kernel/libc/include/stdio.h
+++ kernel/libc/include/stdio.h
@@ -2,12 +2,14 @@
 #define _STDIO_H 1
 
 #include <sys/cdefs.h>
+#include <stdarg.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int printf(const char* __restrict, ...);
+int vsprintf(char*, const char* __restrict, va_list);
 int putchar(int);
 int puts(const char*);
 
--- kernel/libc/stdio/printf.c
+++ kernel/libc/stdio/printf.c
@@ -1,7 +1,6 @@
-#include <stdbool.h>
 #include <stdarg.h>
-#include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 
 static void
 print(const char* data, size_t data_length)
@@ -13,76 +12,13 @@ print(const char* data, size_t data_length)
 int
 printf(const char* restrict format, ...)
 {
+    int written = 0;
+    char buffer[255];
     va_list parameters;
     va_start(parameters, format);
 
-    int written = 0;
-    size_t amount;
-    bool rejected_bad_specifier = false;
-
-    while ( *format != '\0' ) {
-        if ( *format != '%' ) {
-        print_c:
-            amount = 1;
-            while ( format[amount] && format[amount] != '%' )
-                amount++;
-            print(format, amount);
-            format += amount;
-            written += amount;
-            continue;
-        }
-
-        const char* format_begun_at = format;
-
-        if ( *(++format) == '%' )
-            goto print_c;
-
-        if ( rejected_bad_specifier ) {
-        incomprehensible_conversion:
-            rejected_bad_specifier = true;
-            format = format_begun_at;
-            goto print_c;
-        }
-
-        if ( *format == 'c' ) {
-            format++;
-            char c = (char) va_arg(parameters, int /* char promotes to int */);
-            print(&c, sizeof(c));
-        }
-        else if ( *format == 's' ) {
-            format++;
-            const char* s = va_arg(parameters, const char*);
-            print(s, strlen(s));
-        }
-        else if ( *format == 'd' ) {
-            format++;
-            char chr[] = "0123456789";
-            int number = va_arg(parameters, int);
-            int decr, tmp_num;
-
-            if ( number < 0 ) {
-                putchar(0x2D);
-                number*=-1;
-            }
-
-            tmp_num = number;
-
-            for (decr = 1; tmp_num != 0; decr = decr * 10) {
-                tmp_num = tmp_num / 10;
-            }
-
-            for (decr = decr / 10; decr != 0; decr = decr / 10) {
-                putchar(chr[((number / decr) % 10)]);
-            }
-
-            if (number == 0) {
-                putchar('0');
-            }
-        }
-        else {
-            goto incomprehensible_conversion;
-        }
-    }
+    written = vsprintf(&buffer[0], format, parameters);
+    print(buffer, written);
 
     va_end(parameters);
 
