124fee5090f42e62d23c98c51117c2d04dde69b5
Josue David Hernandez Gutierrez <me@josuedhg.com>
2026-07-23 17:34:30 -0600

    kernel/kernel: testing kmalloc
    this is just to test for kmalloc and kfree it is going to be revert in
    the near future.
    
    Signed-off-by: Josue David Hernandez Gutierrez <me@josuedhg.com>

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -29,4 +29,25 @@ void kernel_main(void)
     welcome();
     init_mm();
     heap_init();
+
+    uint32_t *test_allocation = kmalloc(sizeof(uint32_t));
+    *test_allocation = 1000;
+    printf("test_allocation val %d\n", *test_allocation);
+    printf("test_allocation address 0x%08x\n", test_allocation);
+
+    kfree(test_allocation);
+
+    uint32_t *test_allocation2 = kmalloc(sizeof(uint32_t));
+    *test_allocation2 = 1000;
+    printf("test_allocation val %d\n", *test_allocation2);
+    printf("test_allocation address 0x%08x\n", test_allocation2);
+
+    test_allocation = kmalloc(sizeof(uint32_t));
+    *test_allocation = 1000;
+    printf("test_allocation val %d\n", *test_allocation);
+    printf("test_allocation address 0x%08x\n", test_allocation);
+
+
+    kfree(test_allocation2);
+    kfree(test_allocation);
 }
